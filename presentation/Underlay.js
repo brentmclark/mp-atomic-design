d0bc4c50b9ed8e8366003920df518c12892e9af2
Brent M Clark <brentmclark@gmail.com>
2019-07-03 10:13:16 -0500

    code slides

---
+++ presentation/Underlay.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Underlay = styled.div`
+  background: #EEE5E5;
+  padding: 36px 24px;
+  border: 1px solid #DDCECD;
+  display: inline-flex;
+  align-items: center;
+  margin: 0 auto;
+  font-size: 16px;
+  justify-content: center;
+  min-width: 200px;
+  border-radius: 6px;
+`
+export default Underlay
