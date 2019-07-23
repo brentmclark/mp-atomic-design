03b221a0cb8f7be896565f55b85f83c5d6a47455
Brent M Clark <brentmclark@gmail.com>
2019-07-23 08:45:36 -0500

    split slides out individually

---
+++ presentation/deck.js
@@ -0,0 +1,92 @@
+import { dark } from 'mdx-deck/themes'
+import future from '@mdx-deck/themes/future'
+import highlight from '@mdx-deck/themes/syntax-highlighter-prism'
+
+import { slides as slide1 } from './slides/slide1.mdx'
+import { slides as slide2 } from './slides/slide2.mdx'
+import { slides as slide3 } from './slides/slide3.mdx'
+import { slides as slide4 } from './slides/slide4.mdx'
+import { slides as slide5 } from './slides/slide5.mdx'
+import { slides as slide6 } from './slides/slide6.mdx'
+import { slides as slide7 } from './slides/slide7.mdx'
+import { slides as slide8 } from './slides/slide8.mdx'
+import { slides as slide9 } from './slides/slide9.mdx'
+import { slides as slide10 } from './slides/slide10.mdx'
+import { slides as slide11 } from './slides/slide11.mdx'
+import { slides as slide12 } from './slides/slide12.mdx'
+import { slides as slide13 } from './slides/slide13.mdx'
+import { slides as slide14 } from './slides/slide14.mdx'
+import { slides as slide15 } from './slides/slide15.mdx'
+import { slides as slide16 } from './slides/slide16.mdx'
+import { slides as slide17 } from './slides/slide17.mdx'
+import { slides as slide18 } from './slides/slide18.mdx'
+import { slides as slide19 } from './slides/slide19.mdx'
+import { slides as slide20 } from './slides/slide20.mdx'
+import { slides as slide21 } from './slides/slide21.mdx'
+import { slides as slide22 } from './slides/slide22.mdx'
+import { slides as slide23 } from './slides/slide23.mdx'
+import { slides as slide24 } from './slides/slide24.mdx'
+import { slides as slide25 } from './slides/slide25.mdx'
+import { slides as slide26 } from './slides/slide26.mdx'
+import { slides as slide27 } from './slides/slide27.mdx'
+import { slides as slide28 } from './slides/slide28.mdx'
+import { slides as slide29 } from './slides/slide29.mdx'
+import { slides as slide30 } from './slides/slide30.mdx'
+import { slides as slide31 } from './slides/slide31.mdx'
+import { slides as slide32 } from './slides/slide32.mdx'
+import { slides as slide33 } from './slides/slide33.mdx'
+import { slides as slide34 } from './slides/slide34.mdx'
+import { slides as slide35 } from './slides/slide35.mdx'
+import { slides as slide36 } from './slides/slide36.mdx'
+import { slides as slide37 } from './slides/slide37.mdx'
+
+export const themes = [
+  future,
+  highlight,
+  dark,
+  {
+    pre: {
+      fontSize: '20px'
+    },
+  }
+]
+
+export const slides = [
+    ...slide1,
+    ...slide2,
+    ...slide3,
+    ...slide4,
+    ...slide5,
+    ...slide6,
+    ...slide7,
+    ...slide8,
+    ...slide9,
+    ...slide10,
+    ...slide11,
+    ...slide12,
+    ...slide13,
+    ...slide14,
+    ...slide15,
+    ...slide16,
+    ...slide17,
+    ...slide18,
+    ...slide19,
+    ...slide20,
+    ...slide21,
+    ...slide22,
+    ...slide23,
+    ...slide24,
+    ...slide25,
+    ...slide26,
+    ...slide27,
+    ...slide28,
+    ...slide29,
+    ...slide30,
+    ...slide31,
+    ...slide32,
+    ...slide33,
+    ...slide34,
+    ...slide35,
+    ...slide36,
+    ...slide37,
+]
---
+++ presentation/slides/Layout.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Layout = styled.div`
+  width: 100vw;
+  height: 100vw;
+  padding: 3vh 3vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+`
+
+export default Layout
